8f07da9dc1c14b92b6a1848526bf698bc573c1fd
yigitek <yigitek@gmail.com>
2024-04-25 14:38:38 +0200

    BiddingsController CRUD funcs added

--- src/BiddingService/Controllers/BiddingsController.cs
+++ src/BiddingService/Controllers/BiddingsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BiddingService.Data;
 using BiddingService.DTOs;
+using BiddingService.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -37,9 +38,68 @@ public class BiddingsController : ControllerBase
             .Include(s => s.Aircraft)
             .FirstOrDefaultAsync(p => p.Id == id);
 
-        if(bidding == null) return NotFound();
+        if(bidding == null) 
+            return NotFound();
 
         return _mapper.Map<BiddingDto>(bidding);
 
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BiddingDto>> CreateBidding(CreateBiddingDto biddingDto)
+    {
+        var bidding = _mapper.Map<Bidding>(biddingDto);
+        // ADD CURRENT USER AS VENDOR
+        bidding.Vendor = "TestVendor";
+
+        _context.Biddings.Add(bidding);
+
+        var res = await _context.SaveChangesAsync() > 0;
+        if(!res) 
+            return BadRequest("Could not save changes to Database");
+
+        return CreatedAtAction(nameof(GetBiddingById), 
+            new{bidding.Id}, _mapper.Map<BiddingDto>(bidding));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateBidding(Guid id, UpdateBiddingDto updatebiddingDto)
+    {
+        var bidding = await _context.Biddings.Include(s => s.Aircraft)
+            .FirstOrDefaultAsync(s => s.Id == id);
+        
+        if(bidding == null) 
+            return NotFound();
+        //CHECK VENDOR == USERNAME
+        bidding.Aircraft.Company = updatebiddingDto.Company ?? bidding.Aircraft.Company;
+        bidding.Aircraft.ModelNo = updatebiddingDto.ModelNo ?? bidding.Aircraft.ModelNo;
+        bidding.Aircraft.Colour = updatebiddingDto.Colour ?? bidding.Aircraft.Colour;
+        bidding.Aircraft.Milage = updatebiddingDto.Milage ?? bidding.Aircraft.Milage;
+        bidding.Aircraft.BuildDate = updatebiddingDto.BuildDate ?? bidding.Aircraft.BuildDate;
+
+        var res = await _context.SaveChangesAsync() > 0;
+
+        if(res) 
+            return Ok();
+
+        return BadRequest("Couldn't save changes");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteBidding(Guid id)
+    {
+        var bidding = await _context.Biddings.FindAsync(id);
+
+        if(bidding == null)
+            return NotFound();
+
+        _context.Biddings.Remove(bidding);
+
+        var res = await _context.SaveChangesAsync() > 0;
+
+        if(!res)
+            return BadRequest("Couldn't delete");
+
+        return Ok();
     }
 }
--- src/BiddingService/DTOs/UpdateBiddingDto.cs
+++ src/BiddingService/DTOs/UpdateBiddingDto.cs
@@ -4,7 +4,7 @@ public class UpdateBiddingDto
 {
     public string Company { get; set;}
     public string ModelNo { get; set;}
-    public int BuildDate { get; set;}
+    public int? BuildDate { get; set;}
     public string Colour { get; set;}
-    public int Milage { get; set;}
+    public int? Milage { get; set;}
 }
